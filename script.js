e809217184a51a78ee0d03ff93f2eb5fa7ae913c
Lukas Hirsch <hirschlukasa@gmail.com>
2020-03-28 20:08:01 -0500

    update index

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownTime = new Date("Mar 28, 2020 20:07:30").getTime();
+var countDownTime = new Date("Mar 28, 2020 21:00:00").getTime();
 
 var x = setInterval(function(){
 	var now = new Date().getTime();
