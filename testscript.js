bf2200afc956d858ae967f7a8d636591a09f48be
Lukas Hirsch <hirschlukasa@gmail.com>
2020-07-18 18:32:52 -0500

    test

--- testscript.js
+++ testscript.js
@@ -14,7 +14,7 @@ $.ajax({
     
     console.log('data ',data.datetime)
     //console.log("current time: ",currentTime);
-    var countDownTime = 1595115060000;
+    var countDownTime = 1595115540000;
     currentTime = (data.datetime)
     console.log("current time: ",currentTime)
     console.log("countDownTime : ", countDownTime);
