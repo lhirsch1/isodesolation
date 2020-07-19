1983f418b156399f5078da2010d1bd4af31be15a
Lukas Hirsch <hirschlukasa@gmail.com>
2020-07-18 19:21:48 -0500

    script live

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ let countCheck1 = false;
 let countCheck2 = false;
 
 $('.playIn').hide()
+location.reload(true);
 
 $.ajax({
-    url: "https://worldtimeapi.org/api/timezone/America/Thunder_Bay",
+
+    url: "https://worldtimeapi.org/api/timezone/Etc/GMT",
     method: "GET"
 }).then(function(data){
     
-    console.log('data ',data.datetime)
+    //console.log('data ',data.datetime)
     //console.log("current time: ",currentTime);
     var countDownTime = 1595124000000;
     currentTime = (data.datetime)
-    console.log("current time: ",currentTime)
-    console.log("countDownTime : ", countDownTime);
+    // console.log("current time: ",currentTime)
+    // console.log("countDownTime : ", countDownTime);
     var currentdateNorm = new Date(currentTime).getTime();
     var distance = (countDownTime - currentdateNorm) ;
    
-    console.log(distance)
     countDown(distance);
 })
 
@@ -39,34 +40,27 @@ function countDown(distance){
 
     
         $('.timer').text(`${hours} HOUR ${minutes} MINUTES ${seconds} SECONDS UNTIL FILM BEGINS`) 
-        console.log(distance);
+        //console.log(distance);
         $('.playIn').text(`PRESS PLAY IN ${seconds}`) 
         //when countdown is over
         if(distance <= 20000 && !countCheck1){
-            $('.timer').hide()
+            $('.headLineText').hide();
+            $('.timer').hide();
             $('.playIn').show();
             countCheck1 = true;
-            
-            console.log("hi")
             //$('.showTime').html(`<div>ENGAGE FULL SCREEN IMMEDIATELY</div`) ;
             var playIn = document.querySelector('.playIn');
             playIn.style.display = "block"
             
             $('.movie').html('<iframe width="560" height="315" src="https://www.youtube.com/embed/Xb5DPV-iC6k?start=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
-            
-            
-            //hide voting block
-            //document.querySelector('.voteContainer').style.display = 'none';
-            //show film block
-            //show chat
-            //start film
+
             
         }
         if (distance <= 1000){
             $('.playIn').text(`PRESS PLAY NOW`)
             $('.timer').hide();
             // clearInterval(x);
-            console.log("old timer ", x)
+            //console.log("old timer ", x)
             var upDist = distance *-1
             var catchHours = Math.floor((upDist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             var catchMinutes = Math.floor((upDist % (1000 * 60 * 60)) / (1000 * 60));
@@ -74,6 +68,10 @@ function countDown(distance){
             catchHours > 9 ? (catchHours = `${catchHours}`) : (catchHours =`0${catchHours}` )
             catchMinutes > 9 ? (catchMinutes = `${catchMinutes}`) : (catchMinutes =`0${catchMinutes}` )
             catchSeconds > 9 ? (catchSeconds = `${catchSeconds}`) : (catchSeconds =`0${catchSeconds}` )
+
+            if(catchSeconds > 10){
+                $('.playIn').hide()
+            }
 
     
         $('.catchUp').text(`${catchHours}:${catchMinutes}:${catchSeconds} CATCH UP TIME STAMP`) 
